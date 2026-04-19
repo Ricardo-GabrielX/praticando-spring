8d302c76c1334cdc1cf18f3db1e214eda886ce44
Gabriel <gabrieloliveira280807@gmail.com>
2026-04-19 19:53:41 -0300

    feat: adicicionando JwtAuthFilter para integração com Spirng Security

---
+++ src/main/java/ricardo/gabriel/praticando_spring/security/JwtAuthFilter.java
@@ -0,0 +1,44 @@
+package ricardo.gabriel.praticando_spring.security;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter{
+    
+    private final UserDetailsService userDetailsService;
+
+    public JwtAuthFilter(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain filterChain) throws jakarta.servlet.ServletException, java.io.IOException { 
+        
+        String authHeader = request.getHeader("Authorization");
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        String username = JwtUtil.extractUsername(token);
+
+        if( username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+        
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (JwtUtil.validateToken(token)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+                
+            }
+            filterChain.doFilter(request, response);
+        }
+    }
+
+}
