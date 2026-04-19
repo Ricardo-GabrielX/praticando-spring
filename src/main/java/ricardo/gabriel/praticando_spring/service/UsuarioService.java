92af9425a0606779eb6d228bb1972d5352830bef
Gabriel <gabrieloliveira280807@gmail.com>
2026-04-19 18:51:49 -0300

    feat: terminando UsuarioService

--- src/main/java/ricardo/gabriel/praticando_spring/service/UsuarioService.java
+++ src/main/java/ricardo/gabriel/praticando_spring/service/UsuarioService.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service;
 
 import ricardo.gabriel.praticando_spring.model.Usuario;
 import ricardo.gabriel.praticando_spring.repository.UsuarioRepository;
+
+import java.util.Optional;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -22,5 +25,9 @@ public class UsuarioService {
             String senhaCriptografada = passwordEncoder.encode(password);
             Usuario usuario = new Usuario(username, senhaCriptografada);
             return usuarioRepository.save(usuario);
+        }
+
+        public Optional<Usuario> buscarPorUsername(String username) {
+            return usuarioRepository.findByUsername(username);
         }
 }
